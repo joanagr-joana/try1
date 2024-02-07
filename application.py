7617340780772fe900cc3ac17d3710aa17f85fb5
Joana Guasch <joanaguasch@Joanas-MacBook-Pro.local>
2024-02-07 19:13:59 +0100

    Add EB extensions for log directory creation and other log things

--- application.py
+++ application.py
@@ -8,6 +8,19 @@ import os
 
 app = Flask(__name__)
 
+# Create a custom logger
+logger = logging.getLogger(__name__)
+# Create handlers
+c_handler = logging.StreamHandler()
+f_handler = logging.FileHandler('/var/log/flask/error.log')
+# Create formatters and add it to handlers
+c_format = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+f_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+c_handler.setFormatter(c_format)
+f_handler.setFormatter(f_format)
+# Add handlers to the logger
+logger.addHandler(c_handler)
+logger.addHandler(f_handler)
 # Configure logging to write to stdout
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
